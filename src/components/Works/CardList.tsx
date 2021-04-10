1d3438e11423b8107a96e156751f87e63d7910a1
hyunjae <stevysung@gmail.com>
2021-04-10 11:24:33 +0900

    update

--- src/components/Works/CardList.tsx
+++ src/components/Works/CardList.tsx
@@ -5,7 +5,7 @@ import worksData from "../worksData";
 
 const CardList = () => {
   return (
-    <div className='overflow-scroll slide-in-blurred-top w-full flex flex-wrap justify-center items-center mb-percent20'>
+    <div className='overflow-y-scroll slide-in-blurred-top w-full flex flex-wrap justify-center items-center mb-percent20'>
       {worksData.map(({ image, color, title, subTitle }, index) => (
         <Link key={title + subTitle} to={`/works/${index + 1}`}>
           <Card image={image} color={color} title={title} subTitle={subTitle} />
